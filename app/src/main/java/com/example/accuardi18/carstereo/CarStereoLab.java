4614d7cb5736ca5af61ed9d13cd9b58ce24bbc6a
naccuardi <naccuardi@gmail.com>
2016-09-07 14:56:30 -0700

    Initial commit

---
+++ app/src/main/java/com/example/accuardi18/carstereo/CarStereoLab.java
@@ -0,0 +1,13 @@
+package com.example.accuardi18.carstereo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarStereoLab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_stereo_lab);
+    }
+}
